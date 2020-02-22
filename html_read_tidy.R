8cd89bb99bbe3ac6c55ca4965b81de6ac51ee066
Francis Barton <fbarton@alwaysdata.net>
2020-02-22 15:16:27 +0000

    done it!

--- html_read_tidy.R
+++ html_read_tidy.R
@@ -1,6 +1,7 @@
 library(rvest)
 library(stringr)
 library(purrr)
+library(dplyr)
 library(tibble)
 library(conflicted)
 conflict_prefer("pluck", "purrr")
@@ -8,15 +9,18 @@ conflict_prefer("pluck", "purrr")
 # you should add any further URLs to this vector
 urls <- c("http://bocyl.jcyl.es/html/1983/04/02/html/BOCYL-D-02041983-1.do")
 
+# scrape text from the relevant part of the webpage
+# (assume that any additional URLs have the same structure)
 text <- urls %>%
   map(., ~ {xml2::read_html(.) %>%
       rvest::html_nodes("#presentDocumentos p:not([class])") %>% 
       html_text})
 
+# extract a manageable name from the URL and use it to name each text
 names(text) <- urls %>% 
   str_extract_all(., pattern = "(?<=/)BOCYL.*(?=\\.do$)")
 
-# do any manual fixes to broken data here
+# do any manual fixes for errors in source data
 text1 <- text %>% 
   map(., ~
   str_replace_all(., "PARTIDO COMUNISTA DE ESPAÑA PARTIDO COMUNISTA DE CASTILLA- LEON", "2. PARTIDO COMUNISTA DE ESPAÑA PARTIDO COMUNISTA DE CASTILLA- LEON"))
@@ -28,9 +32,10 @@ text2 <- text1 %>%
         str_replace_all(., "^Suplente.*", "") %>% 
         str_c(., collapse = ";") %>% 
         str_split(., pattern = "JUNTA ELECTORAL DE ") %>% 
-        modify_in(., 1, ~ tail(., -1)) %>% 
-        map(., ~ str_split(.,
-                           pattern = ";(?=\\d{1,2}\\.\\s([:upper:]|\\s){2,})") %>% 
+        # modify_in(., 1, ~ tail(., -1)) %>% 
+        map(., ~ tail(., -1) %>% 
+        str_split(.,
+                  pattern = ";(?=\\d{1,2}\\.\\s([:upper:]|\\s){2,})") %>% 
               set_names(str_to_title(map(., 1))) %>% 
               map(., ~ tail(., -1))
             )
@@ -46,12 +51,35 @@ text2[[1]][[1]][["Avila"]] %>%
 
 
 text3 <- text2 %>% 
-  pluck(., 1, 1) %>% 
-  map(.,  ~ str_split(.,
-                      pattern = ";(?=\\d{1,2}\\.\\s)") %>% 
-        set_names(map(., 1) %>% 
-                    str_extract(., pattern = "(?<=\\d{1,2}\\.\\s)[:upper:].*")) %>% 
-        map(.,
-            ~ tail(., -1)))
+  map(., ~
+        map(., ~ 
+              map(., ~ str_split(., pattern = ";(?=\\d{1,2}\\.\\s)") %>% 
+                    set_names(map(., 1) %>% 
+                                str_extract(., pattern = "(?<=\\d{1,2}\\.\\s)[:upper:].*")) %>% 
+                    map(., ~ tail(., -1) %>% 
+                          enframe(., name = "list_position", value = "person_name") %>% 
+                          mutate_at(
+                            vars("person_name"),
+                            ~ str_extract_all(.,
+                                              pattern = "(?<=\\d{1,2}\\.\\s)[:alpha:]+.*"))))))
+
+# test with Avila only first
+text3[[1]][[1]][["Avila"]] %>% 
+  map_df(., c, .id = "political_group")
 
 
+text4 <- text3 %>% 
+  map(., ~
+        map(., ~ 
+              map(., ~
+                    map_df(., c, .id = "political_group"))))
+
+text5 <- text4 %>% 
+  map(., ~
+        map(., ~ 
+              map_df(., c, .id = "territory")))
+
+
+data_frame1 <- text5 %>% 
+  pluck(1, 1) %>% 
+  select(person_name, everything())
